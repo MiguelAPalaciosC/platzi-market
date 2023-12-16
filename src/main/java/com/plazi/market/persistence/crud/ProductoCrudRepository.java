a71d8e2af94694c1e4410fb624591241fed91bbd
MiguelAPalaciosC <a.mapc49@gmail.com>
2023-12-16 10:44:56 -0500

    Query method

--- src/main/java/com/plazi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/plazi/market/persistence/crud/ProductoCrudRepository.java
@@ -1,13 +1,12 @@
 package com.plazi.market.persistence.crud;
 
 import com.plazi.market.persistence.entity.Producto;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
 
-    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
-    List<Producto> getByCategoria(int idCategoria);
+
+    List<Producto> findByIdCategoria(int idCategoria);
 }
