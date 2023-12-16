78261ebe17dda20dc3e09803817a1c8e72f9a3f7
MiguelAPalaciosC <a.mapc49@gmail.com>
2023-12-16 11:03:51 -0500

    Primer repositorio

--- src/main/java/com/plazi/market/persistence/ProductoRepository.java
+++ src/main/java/com/plazi/market/persistence/ProductoRepository.java
@@ -2,9 +2,12 @@ package com.plazi.market.persistence;
 
 import com.plazi.market.persistence.crud.ProductoCrudRepository;
 import com.plazi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -14,5 +17,21 @@ public class ProductoRepository {
 
     public List<Producto> getByCategoria(int idCategoria) {
         return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
